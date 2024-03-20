5c35ddcf23accbeacb2df43634cb12de2587df1e
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-03-20 16:21:23 -0400

    Added genre custom migration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -14,7 +15,26 @@ app.get('/', (req, res) => {
     })
 })
 
+// DATABASE
+const { sequelize } = require('./models')
+//const sequelize = new Sequelize(process.env.DB_CONNECTION)
+const testSequelize = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connection has been established successfully');
+    }   catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }    
+}
+
+//const Band = require('./models/band')
+//await Band.create({
+//    name: "Flatbush Zombied",
+//    genre: "hiphop"
+//})
+
 // LISTEN
 app.listen(process.env.PORT, () => {
+    testSequelize()
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
